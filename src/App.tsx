f2d876e23fc07c631d6af420fd4b91b6d872aa69
Devin Abbott <devinabbott@gmail.com>
2023-09-06 11:51:26 -0700

    Add instant

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+import { init, transact, tx, useQuery } from "@instantdb/react";
+
+const APP_ID = "6a0e56c8-f847-4890-8ae9-06bba6249d34";
+
+init({
+  appId: APP_ID,
+  websocketURI: "wss://api.instantdb.com/runtime/session",
+});
+
+const singletonId = "0c1b1794-87de-4b3c-8f11-b7b66290ffb0";
+
+type CounterData = { counters: { count?: number }[] };
+
+function Counter({ data }: { data: CounterData }) {
+  const counter = data.counters[0];
+  console.log("⚡ + " + JSON.stringify(counter));
+  const count = counter?.count || 0;
+
+  return (
+    <button
+      onClick={() =>
+        transact([tx.counters[singletonId].update({ count: count + 1 })])
+      }
+    >
+      {count}
+    </button>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const query = {
+    counters: {
+      $: {
+        where: {
+          id: singletonId,
+        },
+      },
+    },
+  };
+  const { isLoading, data } = useQuery(query) as {
+    isLoading: boolean;
+    data: CounterData;
+  };
 
   return (
     <>
@@ -17,19 +57,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">{!isLoading && <Counter data={data} />}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
